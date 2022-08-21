aa8c4dd9b5502f5f4bd0f75c9ac83d86ebd8e882
Archana Karri <archansaagari@gmail.com>
2022-08-21 19:31:08 +1000

    Python files

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,19 +5,17 @@ import sys
 #-----------------------------------------------------
 # Set working directory to where this Python file is
 os.chdir(os.path.dirname(__file__))
-file1=os.getcwd() # Current working directory name is stored in variable file
+file=os.getcwd() # Current working directory name is stored in variable file
 # Optional: print the current working directory
 #print("This program is running from: " + os.getcwd())
-#print(file1)
 #-----------------------------------------------------
-pybank_csv=os.path.join("C:/Users/archa/OneDrive/Desktop/homework/Python_challenge/PyBank/Resources", "budget_data.csv")
-#print(pybank_csv)
-with open(pybank_csv,'r') as csv_file:
-     csv_reader= csv.reader(csv_file,delimiter=",")
-     csv_header=next(csv_file)
-     #print(f"Reader:{csv_reader}")
-     #print(f"Header:{csv_header}")
-     rows=[]
+pybank_csv=os.path.join(file,"Resources/budget_data.csv") # Setting path variable to the location where the csv file is stored
+
+with open(pybank_csv,'r') as csv_file: #Opening the csv file in reading mode using the stored path variable
+     csv_reader= csv.reader(csv_file,delimiter=",") # Reading the csv file and setting the delimiter as ","
+     csv_header=next(csv_file) # Retrieving the Csv header information from the Csv file
+     
+     
      total_rows=0
      col=[]
      avg_change=[]
@@ -28,35 +26,32 @@ with open(pybank_csv,'r') as csv_file:
      decprofit=0
      val0=0
      val1=0
-     for row in csv_reader:
-      val1=row[1]
-      rows.append(row)
-      col.append(row[1])
-      if total_rows>=1:
+     for row in csv_reader: # reading the csv_file into row 
+      val1=row[1]      # val1 holds the data in 2nd column of the csv file
+      col.append(row[1]) # list COL holds the data in column 2
+      if total_rows>=1: #This if condition calculates the differnece of  2 conseccutive columns for the whole data given
        change= int(val1)-int(val0)
-       avg_change.append(change)
-       if(change>0):
-          if(incprofit<change):
+       avg_change.append(change)  # the difference calculated is stored in the avg_change list
+       if(change>0):            # this if compares  the change and if the change is more than the previous one, then increased profit data is stored
+          if(incprofit<change): # Subsequent date value will be stored in the incdate variable
            incprofit=change
            incdate=row[0]
           
-       else:
+       else:                    #this if compares  the change and if the change is less than the previous one, then decreased profit data is stored
           if(decprofit>change):
            decprofit=change
-           decdate=row[0]
+           decdate=row[0]        # Subsequent date value will be stored in the decdate variable
           
-      total_rows=total_rows+1
-      val0=row[1]
-      avg_changeval=avg_changeval+int(change)
-      #print(col)
-      #total_profitloss=total_profitloss
+      total_rows=total_rows+1  # counts the total rows by incrementing by 1 each time the for loop is executed
+      val0=row[1]               # val0 stores the value of profit/Loss for comparing with the next row data
+      avg_changeval=avg_changeval+int(change) # avg_changeval is calcualting the total for all the data
+      
      
      for i in col:
-      #print(i)
-      total_profitloss=total_profitloss+int(i)
+         total_profitloss=total_profitloss+int(i)   # for all data in col, total_profitloss is calculating the total
       
-     avg_changeval=round(avg_changeval/(total_rows-1),2)
-      #print(avg_change)
+     avg_changeval=round(avg_changeval/(total_rows-1),2)   # calculating the mean change for the given data
+     # this section prints the oputput in the terminal in the desired format
      print("Financial Analysis") 
      print("----------------------------------------------------------------")
      print("Total months: " + str(total_rows))
@@ -64,7 +59,8 @@ with open(pybank_csv,'r') as csv_file:
      print("Average change: $"+ str(avg_changeval))
      print("Greatest Increase in profits:"+ str(incdate) + " ($" +str(incprofit)+")")
      print("Greatest Derease in profits:"+ str(decdate) + " ($" +str(decprofit)+ ")")
-
+     
+      # this section writes the oputput in a text file which is in the Analysis folder
      sys.stdout = open("Analysis/test.txt", "w")
      print("Financial Analysis") 
      print("----------------------------------------------------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,66 +6,82 @@ from tkinter import N
 #-----------------------------------------------------
 # Set working directory to where this Python file is
 os.chdir(os.path.dirname(__file__))
-file1=os.getcwd() # Current working directory name is stored in variable file
+file=os.getcwd() # Current working directory name is stored in variable file
 # Optional: print the current working directory
 #print("This program is running from: " + os.getcwd())
-#print(file1)
 #-----------------------------------------------------
-pypoll_csv=os.path.join("C:/Users/archa/OneDrive/Desktop/homework/Python_challenge/PyPoll/Resources", "election_data.csv")
-#print(pybank_csv)
-with open(pypoll_csv,'r') as csv_file:
-     csv_reader= csv.reader(csv_file,delimiter=",")
-     csv_header=next(csv_file)
-     #print(f"Reader:{csv_reader}")
-     #print(f"Header:{csv_header}")
-     rows=[]
-     total_rows=0
+pypoll_csv=os.path.join(file,"Resources/election_data.csv") # Sets the path variable to get the location of csv file by using join 
+with open(pypoll_csv,'r') as csv_file:                  # opening csv file in read mode using the path variable
+     csv_reader= csv.reader(csv_file,delimiter=",")      # csv reader variable declaration
+     csv_header=next(csv_file)                           #sv_header variable declaration
+     rows=[]                                      #Declaration of lists and varaibles that will be used in the program
+     total_rows=0  
      col=[]
-     uniqval=''
-     avg_change=[]
-     avg_changeval=0
-     change=0
-     total_profitloss=0
-    
-     val0=0
-     val1=0
-     for row in csv_reader:
-      val1=row[1]
-      rows.append(row)
-      if total_rows==0:
-       col.append(row[2])
-      if total_rows>=1:
-        if uniqval!=row[2]:
-          
-             col.append(row[2])
-         
-      total_rows=total_rows+1
-      uniqval=row[2]
-      #avg_changeval=avg_changeval+int(change)
-      #print(col)
-      #total_profitloss=total_profitloss
-     
-     
+     uniqval=''                                     # This variable holds the name of unique variable the contestant
+     county=[]                                        #this list holds the list of County in the given data
+     names=[]
+     uniqcounty=''
+     count=0
+     votecount=[]
+     total_votes=0
+     list=[]
+
+
+     def countvotes(list, x,votes):          # Defiing function to count the total votes by taking list , contestant name and total votes casted
+      count = 0
+      local=0
+      percent=0
+      for ele in list:                   #This loop counts the total votes casted for each contestant
+        if (ele == x):
+            count = count + 1
+      percent= (count/votes)*100         # Counts the total percentage of votes for each contestant
+      print("Total votes for "+ str(x) +" : "+str(round(percent,3))+ "%"+" ("+ str(count)+")") # Command to print the total votes for each contestant
+      return(count)                        # countvotes is returning the count value to the variable where the function is called
       
-     avg_changeval=round(avg_changeval/(total_rows-1),2)
-      #print(avg_change)
+     for row in csv_reader:
+      #if uniqcounty!=row[1]:              optional code if name of county needs to be captured
+       #county.append(row[1])
+      list.append(row[2])                # This list holds the data of all votes casted for each contestant
+      if uniqval==row[2]:         
+        count=count+1
+      
+      if uniqval!=row[2]:              # This if loops adds unique contestant values the list named col
+        if row[2] not in col:
+         col.append(row[2]) 
+      total_rows=total_rows+1            # total_rows count the total number of votes casted in the election
+      #uniqcounty=row[1]
+      uniqval=row[2] 
+
+      #This section prints the output in the terminal in the desired format
      print("Election results") 
      print("----------------------------------------------------------------")
      print("Total votes: " + str(total_rows))
-     print(col)
-    # print("Total Profit/Loss: $"+ str(total_profitloss))
-    # print("Average change: $"+ str(avg_changeval))
-    # print("Greatest Increase in profits:"+ str(incdate) + " ($" +str(incprofit)+")")
-    # print("Greatest Derease in profits:"+ str(decdate) + " ($" +str(decprofit)+ ")")
-
+     print("----------------------------------------------------------------")
+     result=0
+     for i in col: 
+      count=countvotes(list,i,total_rows)  
+      if(result<count):                                                        # comparing the votes for each contestant and announing  the winner with highest votes
+        result=count
+        winner=i
+     print("----------------------------------------------------------------")
+     print("Winner is: "+ winner)
+     print("----------------------------------------------------------------")
+      
+#   This section writes the output in a text file stored in the Analysis folder
      sys.stdout = open("Analysis/test.txt", "w")
      print("Election results") 
      print("----------------------------------------------------------------")
      print("Total votes: " + str(total_rows))
-     #print("Total Profit/Loss: $"+ str(total_profitloss))
-     #print("Average change: $"+ str(avg_changeval))
-     #print("Greatest Increase in profits:"+ str(incdate) + " ($" +str(incprofit)+")")
-     #print("Greatest Derease in profits:"+ str(decdate) + " ($" +str(decprofit)+ ")")
+     print("----------------------------------------------------------------")
+     result=0
+     for i in col: 
+      count=countvotes(list,i,total_rows)  
+      if(result<count):
+        result=count
+        winner=i
+     print("----------------------------------------------------------------")  # comparing the votes for each contestant and announing  the winner with highest votes
+     print("Winner is: "+ winner)
+     print("----------------------------------------------------------------")
      sys.stdout.close()  
      
     
